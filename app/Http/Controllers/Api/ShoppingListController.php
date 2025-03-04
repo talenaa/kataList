be48ed17a10239b67b3b9fe8507a9f00a7a5f96a
talenaa <talenabarbosa7@gmail.com>
2025-03-04 21:54:16 +0100

    feat: added api controller

---
+++ app/Http/Controllers/Api/ShoppingListController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShoppingListController extends Controller
+{
+    public function index()
+    {
+        $list = ShoppingList::all();
+        return response()->json($list, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $existing = ShoppingList::where('name', '=', $request->name)->first();
+
+        if ($existing ){
+            return 'producto existente';
+        }
+
+        $list = ShoppingList::create([
+            'name' => $request->name,
+        ]);
+
+        $list->save();
+        return response()->json($list, 200);
+    }
+
+    public function show(string $id)
+    {
+        $list = ShoppingList::find($id);
+        return response()->json($list, 200);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $list = ShoppingList::find($id);
+
+        $list -> update([
+            'name' => $request->name,
+        ]);
+        $list->save();
+        return response()->json($list, 200);
+    }
+
+    public function destroy(string $id)
+    {
+        $list = ShoppingList::find($id);
+        $list->delete();
+    }
+
+    public function destroyAll()
+    {
+        $list = ShoppingList::all();
+
+        foreach($list as $element){
+            $element->delete();
+        }
+    }
+}
